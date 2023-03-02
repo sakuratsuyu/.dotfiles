5d8456c3221a196bcdcc1c69e81dc59da793bae9
sakuratsuyu <sakuratsuyu@outlook.com>
2023-03-02 11:23:59 +0800

    Modify nvim undotree

--- nvim-lua/lua/plugins/undotree.lua
+++ nvim-lua/lua/plugins/undotree.lua
@@ -1,9 +1,10 @@
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 local fn = vim.fn
 local cmd = vim.api.nvim_command
 
-keymap.set("n", "<M-u>", ":UndotreeToggle<CR>")
-keymap.set("v", "<M-u>", ":UndotreeToggle<CR>")
+keymap.set('n', '<M-u>', ':UndotreeToggle<CR>', opts)
+keymap.set('v', '<M-u>', ':UndotreeToggle<CR>', opts)
 
 fn.system('mkdir -p ~/.config/nvim/tmp/backup')
 fn.system('mkdir -p ~/.config/nvim/tmp/undo')
