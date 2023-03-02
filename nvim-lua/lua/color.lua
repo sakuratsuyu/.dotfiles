1dd2e9d0d4fb313440244884d948229c987b63d7
sakuratsuyu <sakuratsuyu@outlook.com>
2023-03-02 11:21:47 +0800

    Configure nvim everforest

--- nvim-lua/lua/color.lua
+++ nvim-lua/lua/color.lua
@@ -5,12 +5,13 @@ local fn = vim.fn
 opt.cursorline = true
 opt.termguicolors = true
 
-g.everforest_diagnostic_line_highlight = 1
-
 -- Color Scheme
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/everforest'
 if fn.empty(vim.fn.glob(install_path)) <= 0 then
     vim.cmd('colorscheme everforest')
+    opt.background = 'dark'
+    g.everforest_background = 'medium'
+    g.everforest_diagnostic_line_highlight = 1
 end
 
 -- Diagnostic Line
