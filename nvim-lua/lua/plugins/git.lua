2e9a0c25894560213682e2b1227ee7f3c13bcb2b
sakuratsuyu <sakuratsuyu@outlook.com>
2023-04-06 22:29:09 +0800

    [nvim] Update gitgutter keymap.

--- nvim-lua/lua/plugins/git.lua
+++ nvim-lua/lua/plugins/git.lua
@@ -1,6 +1,11 @@
+local g = vim.g
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 keymap.set('n', '<M-g>', ':GitGutterLineHighlightsToggle<CR>', opts)
 keymap.set('v', '<M-g>', ':GitGutterLineHighlightsToggle<CR>', opts)
 keymap.set('i', '<M-g>', '<ESC>:GitGutterLineHighlightsToggle<CR>a', opts)
+
+g.gitgutter_map_keys = 0
+keymap.set('n', '[c', '<Plug>GitGutterPrevHunk', opts)
+keymap.set('n', ']c', '<Plug>GitGutterNextHunk', opts)
