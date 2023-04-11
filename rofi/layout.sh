d411e87ab95c01efbff8b1f98efc9b12fc9c917a
sakuratsuyu <sakuratsuyu@outlook.com>
2023-04-12 02:32:08 +0800

    [rofi] Simplify `layout.sh`.

--- rofi/layout.sh
+++ rofi/layout.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 case "$*" in
-    "Layout 1")
-        ~/Scripts/Screenlayout/Layout_1.sh
+    Layout_*.sh)
+        ~/Scripts/Screenlayout/$*
+        ~/.config/polybar/launch.sh > /dev/null
         ;;
-    "Layout 2")
-        ~/Scripts/Screenlayout/Layout_2.sh
-        ;;
-    "Layout 3")
-        ~/Scripts/Screenlayout/Layout_3.sh
-        ;;
-    "")
-        echo Layout 1
-        echo Layout 2
-        echo Layout 3
+    *)
+        ls ~/Scripts/Screenlayout
         ;;
 esac
