d687969f93c20f15bb34c99a3111f8e1268ba1fd
sakuratsuyu <sakuratsuyu@outlook.com>
2023-04-14 18:25:00 +0800

    [fish] Quit ranger with `Q` to change the directory.

---
+++ fish/functions/ranger.fish
@@ -0,0 +1,13 @@
+function ranger
+	set tempfile (mktemp -t tmp.XXXXXX)
+	set command_argument "map Q chain shell echo %d > $tempfile; quitall"
+	command ranger --cmd="$command_argument" $argv
+	if test -s $tempfile
+		set ranger_pwd (cat $tempfile)
+		if test -n $ranger_pwd -a -d $ranger_pwd
+			builtin cd -- $ranger_pwd
+		end
+	end
+
+	command rm -f -- $tempfile
+end
