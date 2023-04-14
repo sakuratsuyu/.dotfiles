3737ec076ac95cb330371bc0b1754a2af4f2b47b
sakuratsuyu <sakuratsuyu@outlook.com>
2023-04-14 18:17:33 +0800

    [fish] Modify prompt colors and add `arch` icon.

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,17 +1,24 @@
 function fish_mode_prompt
+    set_color ffffff -b 1793d1
+    echo -n '  '
+    set_color normal -b normal
+
     if test $fish_key_bindings = fish_vi_key_bindings
         or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
-        # Everforest Pattern
+        set_color 000000
         switch $fish_bind_mode
             case default
                 set_color -b a7c080
             case insert
-                set_color -b d3c6aa
+                set_color -b f0ce8e
             case visual
-                set_color -b e67e80
+                set_color -b 98b3c9
+            case replace
+                set_color -b 7f33df
             case replace_one
-                set_color -b e69875
+                set_color -b bea6c6
         end
-        echo -n " "
+        echo -n "  "
+        set_color -b normal
     end
 end
