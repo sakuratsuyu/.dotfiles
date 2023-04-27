79e21851bd4f4ba36ca2f59211ef82062d029df9
sakuratsuyu <sakuratsuyu@outlook.com>
2023-04-27 17:57:23 +0800

    [fish] Update for not making error.

--- fish/functions/ct.fish
+++ fish/functions/ct.fish
@@ -1,3 +1,8 @@
 function ct
-    /bin/rm -rf ~/.trash_bin/.*
+    if test (math (count (ls -a ~/.trash_bin/))-(count (ls ~/.trash_bin/))) -gt 2
+        /bin/rm -rf ~/.trash_bin/.*
+    end
+    if test (count (ls ~/.trash_bin/)) -gt 0
+        /bin/rm -rf ~/.trash_bin/*
+    end
 end
