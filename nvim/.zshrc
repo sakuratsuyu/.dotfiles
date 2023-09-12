b4f2b8d3d24d698d935d6e56574f626e4ae7c536
sakuratsuyu <sakuratsuyu@outlook.com>
2023-09-12 13:24:54 +0800

    [Update] Remove linux tool config.

--- nvim/.zshrc
+++
@@ -1,1 +0,0 @@
-/Users/sakur/.dotfiles/zsh/.zshrc
--- polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env bash
-
-polybar-msg cmd quit
-
-MONITOR=eDP-1 polybar --reload primary &
-MONITOR=DP-1 polybar --reload primary &
-MONITOR=HDMI-1 polybar --reload secondary &
